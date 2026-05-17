0c9dec10a6e2f6e95190ff959283ac3784a320b0
Aakash Ranjan <92005585+AakashRanjan07@users.noreply.github.com>
2026-05-17 09:55:43 +0530

    Add utility functions to sandbox

---
+++ utils.ts
@@ -0,0 +1,28 @@
+/**
+ * Generic utility functions for the sandbox.
+ */
+
+export function debounce<T extends (...args: any[]) => void>(
+  func: T,
+  wait: number
+): (...args: Parameters<T>) => void {
+  let timeout: NodeJS.Timeout | null = null;
+  return (...args: Parameters<T>) => {
+    if (timeout) clearTimeout(timeout);
+    timeout = setTimeout(() => func(...args), wait);
+  };
+}
+
+export function throttle<T extends (...args: any[]) => void>(
+  func: T,
+  limit: number
+): (...args: Parameters<T>) => void {
+  let inThrottle = false;
+  return (...args: Parameters<T>) => {
+    if (!inThrottle) {
+      func(...args);
+      inThrottle = true;
+      setTimeout(() => (inThrottle = false), limit);
+    }
+  };
+}
